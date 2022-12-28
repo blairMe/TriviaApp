915da83e0128bd9aa4ec99c00bda17f9f2554291
blairMe <felixanachoni@gmail.com>
2022-12-28 23:09:27 +0300

    TriviaApp

---
+++ app/src/main/java/bfa/blair/triviaapp/model/Question.kt
@@ -0,0 +1,3 @@
+package bfa.blair.triviaapp.model
+
+class Question : ArrayList<QuestionItem>()
---
+++ app/src/main/java/bfa/blair/triviaapp/model/QuestionItem.kt
@@ -0,0 +1,8 @@
+package bfa.blair.triviaapp.model
+
+data class QuestionItem(
+    val answer: String,
+    val category: String,
+    val choices: List<String>,
+    val question: String
+)
